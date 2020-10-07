fdc6bb5ea5cbcce60ad7b389a584f294221a7b05
Filipe Kramer <71034131+FilipeKramer-12@users.noreply.github.com>
2020-10-07 17:02:15 -0300

    add files final project.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// SLIDER BANNER
+new SimpleSlide({
+    slide: 'slider-banner',
+    nav: true, 
+    time: 4000, 
+});
+
+// SLIDER MENU 
+new SimpleSlide({
+  slide: 'pizza-slider',
+  nav: false,
+  time: 4000, 
+});
+
+new SimpleSlide({
+  slide: 'bebidas-slider',
+  nav: false,
+  time: 3500, 
+});
+
+// SLIDER MENU MOBILE
+new SimpleSlide({
+  slide: 'pizza-mobile-slider',
+  nav: true,
+  time: 3000, 
+});
+
+new SimpleSlide({
+  slide: 'bebidas-mobile-slider',
+  nav: true,
+  time: 2500, 
+});
+
+
